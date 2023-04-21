c4705f1094eb42f353d17fb0523dbace6c71e21f
SSPENELOPE <typoepping@gmail.com>
2023-04-21 08:54:47 -0400

    Added all controllers and Interfaces

--- src/main/java/com/technews/technewsjavaapi/repository/CommentRepository.java
+++ src/main/java/com/technews/technewsjavaapi/repository/CommentRepository.java
@@ -1,12 +1,12 @@
 package com.technews.technewsjavaapi.repository;
 
-import com.technews.technewsjavaapi.model.Post;
+import com.technews.technewsjavaapi.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Post, Integer> {
-    List<Post> findAllPostsByUserId(Integer id) throws Exception;
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findAllPostsByUserId(Integer id) throws Exception;
 }
